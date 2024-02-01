9f9bbd5675bdbb7ba3a0570b62595dca9daede09
Mo Shihab <Mohammedshihab6969@gmail.com>
2024-02-01 01:17:43 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import Intro from './components/Intro';
+function App() {
+  return (
+    <div className="h-[4000px]">
+      <Header />
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
